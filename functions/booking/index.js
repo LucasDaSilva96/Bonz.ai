324c027da83aea440b3b1abb0aa92aecd368da84
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-09-10 12:51:03 +0200

    Implement create new booking function

--- functions/booking/index.js
+++ functions/booking/index.js
@@ -1,10 +1,34 @@
 // Som en gäst vill jag kunna boka ett rum för att säkerställa min vistelse på hotellet.
 // Som en gäst vill jag få en bekräftelse när jag har bokat ett rum för att veta att min bokning har gått igenom.
 
+const { createBooking } = require('../../lib/createBooking');
+const { sendError, sendResponse } = require('../../responses');
+const { db } = require('../../services/db');
+
 exports.handler = async (event, context) => {
-  console.log('Hello, world!');
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello, world!' }),
-  };
+  const body = JSON.parse(event.body);
+  const { name, email, amount_of_people, check_in, check_out } = body;
+
+  if (!name || !email || !amount_of_people || !check_in || !check_out) {
+    return sendError(400, 'Missing required fields');
+  }
+
+  try {
+    const booking = await createBooking(
+      name,
+      email,
+      amount_of_people,
+      check_in,
+      check_out
+    );
+
+    await db.put({
+      TableName: 'booking-db',
+      Item: booking,
+    });
+
+    return sendResponse(booking);
+  } catch (error) {
+    return sendError(500, error.message);
+  }
 };
