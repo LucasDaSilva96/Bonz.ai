f49ee616acb33c0be69d06d9be3de5f3d3df9fe1
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-09-12 15:25:11 +0200

    Fixe import of db

--- functions/overview/index.js
+++ functions/overview/index.js
@@ -1,17 +1,17 @@
 // Som receptionist vill jag kunna se alla bokningar som gjorts för att få en överblick över hur beläggningen av hotellet ser ut.
 
-const { db } =requier('./services/db.js');
+const { db } = require('../../services/db');
 
 module.exports.handler = async (event) => {
   const params = {
-    TableName: 'booking-db',  // DynamoDB-tabellen för bokningar
+    TableName: 'booking-db', // DynamoDB-tabellen för bokningar
   };
 
   try {
     const data = await db.scan(params).promise(); // Skannar tabellen
-   return sendResponse(data.Items);
+    return sendResponse(data.Items);
   } catch (err) {
     console.error('Error scanning table:', err);
-   return sendError(500, 'Could not retrive bookings')
+    return sendError(500, 'Could not retrive bookings');
   }
 };
