80a25189f5911b2522758e01bf09251649d68cc1
Rebecca <rebeccaaliina@gmail.com>
2024-09-12 14:03:52 +0200

    förkortad kod
    kortat ner overview kod

--- functions/overview/index.js
+++ functions/overview/index.js
@@ -1,7 +1,6 @@
 // Som receptionist vill jag kunna se alla bokningar som gjorts för att få en överblick över hur beläggningen av hotellet ser ut.
 
-const AWS = require('aws-sdk'); // Laddar AWS SDK
-const db = new AWS.DynamoDB.DocumentClient(); // Skapar DynamoDB-klienten
+const { db } =requier('./services/db.js');
 
 module.exports.handler = async (event) => {
   const params = {
@@ -10,17 +9,9 @@ module.exports.handler = async (event) => {
 
   try {
     const data = await db.scan(params).promise(); // Skannar tabellen
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        bookings: data.Items, // Returnerar bokningarna i JSON-format
-      }),
-    };
+   return sendResponse(data.Items);
   } catch (err) {
     console.error('Error scanning table:', err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Could not retrieve bookings' }),
-    };
+   return sendError(500, 'Could not retrive bookings')
   }
 };
